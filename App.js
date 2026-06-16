e97935d5644461eba6ba19cced83eb41dffd4261
srimathiv-lab <kit25csebsrimathiv@gmail.com>
2026-06-16 20:00:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Task Manager</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
